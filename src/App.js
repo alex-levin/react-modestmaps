77d72a04926362cb5a7ef8ea69c72d84be01ca59
alex.levin <alexlevinb@gmail.com>
2019-04-13 08:34:59 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+import MM from 'modestmaps';
+import MMFollower from './follower-canvas';
+
+class App extends Component {
+    componentDidMount() {
+        var layer = new MM.TemplatedLayer(
+            'http://osm-bayarea.s3.amazonaws.com/{Z}-r{Y}-c{X}.jpg'
+        );
+
+        let map = new MM.Map('root', layer, new MM.Point(690, 360));
+        MM.Follower = MMFollower(MM);
+        let f = new MM.Follower(
+            map,
+            new MM.Location(37.81153, -122.2666097),
+            'Broadway and Grand'
+        );
+        map.setCenterZoom(new MM.Location(37.81153, -122.2666097), 14);
+    }
+
+    render() {
+        return <div>Hello</div>;
+    }
+}
+
+export default App;
